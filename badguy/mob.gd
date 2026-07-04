017d730a415f8e2c2d92fea5514f0aed067294be
sadbunnygit <danelle06@outlook.com>
2026-07-04 02:44:09 -0400

    centered interact thing

--- badguy/mob.gd
+++ badguy/mob.gd
@@ -12,13 +12,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$AnimatedSprite2D.animation = "default"
 
-
+var already_clicked = false
 func _on_control_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and !already_clicked:
 			print_debug("I've been left clicked D:")
+			already_clicked = true
 			print_debug("starting interaction options")
 			var irt = interact.instantiate()
 			add_child(irt)
-			irt.global_position = self.global_position 
+			print_debug("self.position: ", self.position.x, ", ", self.position.y)
+			print_debug("self.global_position: ", self.global_position.x, ", ", self.global_position.y)
+			irt.position = Vector2(-100,0)
 	
